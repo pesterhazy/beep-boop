22ea4a4feea54f9b98a8d9bd63c742791d8100b8
Paulus Esterhazy <pesterhazy@gmail.com>
2023-03-14 11:21:09 +0100

    WIP

--- src/beep_boop/main.clj
+++ src/beep_boop/main.clj
@@ -1,4 +1,16 @@
-(ns beep-boop.main)
+(ns beep-boop.main
+  (:require [babashka.process :refer [sh shell process]]
+            [babashka.fs :as fs]))
 
-(defn -main []
-  (prn :beep-boop))
+(def root (-> *file* fs/parent fs/parent fs/parent))
+
+(defn play [fname]
+  (process "afplay" (str root "/sounds/" fname ".wav")))
+
+(defn -main [& args]
+  (play "start")
+  (let [prc (apply shell {:continue true} args)]
+    (if (zero? (:exit prc))
+      (play "success")
+      (play "fail"))
+    (System/exit (:exit prc))))
