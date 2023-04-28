ba1f085711760cadbe69a15cac2da2cabd90553a
Paulus Esterhazy <pesterhazy@gmail.com>
2023-04-28 14:06:30 +0200

    WIP

--- src/beep_boop/main.clj
+++ src/beep_boop/main.clj
@@ -7,9 +7,16 @@
 (defn play [fname]
   (process "afplay" (str root "/sounds/" fname ".wav")))
 
+(defmacro time-ret
+  [expr]
+  `(let [start# (. System (nanoTime))
+         ret# ~expr]
+     [(/ (double (- (. System (nanoTime)) start#)) 1000000.0) ret#]))
+
 (defn -main [& args]
   (play "start")
-  (let [prc (apply shell {:continue true} args)]
+  (let [[elapsed-ms prc] (time-ret (apply shell {:continue true} args))]
+    (println "... " elapsed-ms "ms")
     (if (zero? (:exit prc))
       (play "success")
       (play "fail"))
