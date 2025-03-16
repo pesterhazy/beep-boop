ba8cf0971915112bd48ef9c17b7f51d7cfd6ce4c
Paulus Esterhazy <pesterhazy@gmail.com>
2025-03-16 17:17:21 +0100

    WIP

--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,25 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const soundFilePath = path.resolve(__dirname, "../sounds/sounds_start.wav");
 
-// Function to play sound on macOS using spawnSync
-function playSound(filePath: string): void {
-  try {
-    const result = spawnSync("afplay", [filePath], { stdio: "inherit" });
+type SoundType = "start" | "failure" | "success";
 
-    if (result.error) {
-      console.error(`Error playing sound: ${result.error.message}`);
-    } else if (result.status !== 0) {
-      console.log(`Sound process exited with code ${result.status}`);
-    } else {
-      console.log("Sound played successfully");
-    }
-  } catch (error) {
-    console.error(`Exception playing sound: ${error}`);
+function playSound(type: SoundType): void {
+  const soundFiles: Record<SoundType, string> = {
+    start: path.resolve(__dirname, "../sounds/start.wav"),
+    failure: path.resolve(__dirname, "../sounds/failure.wav"),
+    success: path.resolve(__dirname, "../sounds/success.wav"),
+  };
+
+  const filePath = soundFiles[type];
+
+  const result = spawnSync("afplay", [filePath], { stdio: "inherit" });
+
+  if (result.error) {
+    throw new Error("Error running command");
+  } else if (result.status !== 0) {
+    throw new Error("Nonzero exit status");
   }
 }
 
-playSound(soundFilePath);
+playSound("start");
